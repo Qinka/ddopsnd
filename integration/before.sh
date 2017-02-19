0ee6eacdb87b952403528fb087cf959cd29f551b
Qinka <qinka@live.com>
2017-02-19 14:05:38 +0800

    update

--- integration/before.sh
+++ integration/before.sh
@@ -33,3 +33,4 @@ export PATH=$HOME/.local/bin:$PATH
 travis_retry curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
 echo setting up ghc
 export PATH=/opt/ghc/$GHC_VER/bin:$PATH
+stack config set system-ghc --global true
